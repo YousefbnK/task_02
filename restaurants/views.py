2668d868237b3b3ca394c5a71901664d88610b3e
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-01-12 19:34:35 +0300

    Django success

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def helloworld(request):
+	context = {
+			"msg": "Hello World!"
+	}
+	return render(request, "start.html", context)
